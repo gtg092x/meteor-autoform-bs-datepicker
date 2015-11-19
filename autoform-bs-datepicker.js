4f122dea1c7fb7aa783034e198a975ff21f9d623
Matthew Drake <mdrake@mediadrake.com>
2015-11-19 09:38:00 -0800

    Fixed empty string validation issue
    https://github.com/aldeed/meteor-autoform-bs-datepicker/issues/15

--- autoform-bs-datepicker.js
+++ autoform-bs-datepicker.js
@@ -60,7 +60,7 @@ Template.afBootstrapDatepicker.rendered = function () {
     // set field value
     if (data.value instanceof Date) {
       $input.datepicker('setUTCDate', data.value);
-    } else if (typeof data.value === "string") {
+    } else if (typeof data.value === "string" && data.value) {
       $input.datepicker('update', data.value);
     }
 
